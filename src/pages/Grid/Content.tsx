81a15e14a872275b33a016964058f0170cbb745a
Nagellan <ireknazmievirek@gmail.com>
2025-10-22 00:41:20 +0400

    Optimize images

--- src/pages/Grid/Content.tsx
+++ src/pages/Grid/Content.tsx
@@ -39,7 +39,7 @@ export const Content = () => {
 	const onLoaded = useLoadOnScroll(wrapperRef, onScrollEnd);
 
 	useEffect(() => {
-		api.getPhotos(page, 40).then((response) => {
+		api.getPhotos(page, 80).then((response) => {
 			setPhotos((prevPhotos) => [...prevPhotos, ...response.photos]);
 			onLoaded();
 		});
@@ -57,8 +57,11 @@ export const Content = () => {
 						.map((photo) => (
 							<Photo
 								key={photo.id}
-								src={photo.src.medium}
+								src={photo.src.original}
 								alt={photo.alt}
+								srcSet={`${photo.src.original}?auto=compress&cs=tinysrgb&w=150&loading=lazy 150w, ${photo.src.original}?auto=compress&cs=tinysrgb&w=300&loading=lazy 300w, ${photo.src.original}?auto=compress&cs=tinysrgb&w=400&loading=lazy 400w, ${photo.src.original}?auto=compress&cs=tinysrgb&w=600&loading=lazy 600w, ${photo.src.original}?auto=compress&cs=tinysrgb&w=800&loading=lazy 800w, ${photo.src.original}?auto=compress&cs=tinysrgb&w=1200&loading=lazy 1200w, ${photo.src.original}?auto=compress&cs=tinysrgb&w=1600&loading=lazy 1600w`}
+								sizes="(width <= 425px) 425px, (width <= 768px) 384px, (width <= 1440px) 240px, (width <= 2560) 215px"
+								loading="lazy"
 							/>
 						))}
 				</Column>
