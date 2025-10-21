71f7715776f304c16d948f2b040a3082c2470c32
Nagellan <ireknazmievirek@gmail.com>
2025-10-21 19:53:31 +0400

    Support path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -41,4 +42,9 @@ export default defineConfig({
 			},
 		}),
 	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
