971c2c8895f1b8ff2098cd95a27384d35f60687c
Nagellan <ireknazmievirek@gmail.com>
2025-10-24 16:08:57 +0400

    Fix page error component

--- src/components/page/PageError.tsx
+++ src/components/page/PageError.tsx
@@ -1,22 +1,19 @@
 import { useCallback } from 'react';
-import { useNavigate } from 'react-router';
 
 import { Button } from '@/components/ui/Button';
 
 import { PageStatus } from './PageStatus';
 
 export const PageError = () => {
-	const navigate = useNavigate();
-
 	const onClick = useCallback(() => {
-		navigate('/');
-	}, [navigate]);
+		window.location.reload();
+	}, []);
 
 	return (
 		<PageStatus
 			title="Page crashed!"
-			description="Something went wrong. Please, refresh page or navigate to homepage."
-			action={<Button onClick={onClick}>Go home</Button>}
+			description="Something went wrong. Please, try reloading page."
+			action={<Button onClick={onClick}>Reload</Button>}
 		/>
 	);
 };
